61749143584b92af2ee09472e3270a67ae98fd0f
Rory Gee <rorygeesbe@gmail.com>
2019-12-06 21:46:23 +0000

    added youtube embed syntax

--- src/index.php
+++ src/index.php
@@ -5,9 +5,9 @@
         </div>
     </style>
     <head>
-
     </head>
     <body>
-
+		<?php
+			echo"<iframe width='560' height='315' src='https://www.youtube.com/embed/"+someVariable+"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
     </body>
 </html>
