b8316ea2fa2416323963dac42e14628bc16c846c
Rory Gee <rorygeesbe@gmail.com>
2019-12-06 22:23:09 +0000

    Basic Video Retrieval

--- src/index.php
+++ src/index.php
@@ -1,15 +1,25 @@
 <html>
-    <style>
-        <div> 
-            background-image: url('../img/background.jpg');
-        </div>
-    </style>
     <head>
+		<title>HACK Keele | Music Voting System</title>
+		<style>
+			<div> 
+				background-image: url('../img/background.jpg');
+			</div>
+		</style>
+
     </head>
     <body>
-		<?php
-			$ID = "OWc1jaycOlQ";
-			echo"<iframe width='560' height='315' src='https://www.youtube.com/embed/".$ID."' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+		<form action="/index.php">
+			Video ID:<br>
+			<input type="text" name="videoID" value="OWc1jaycOlQ">
+			<br>
+			<input type="submit" value="Submit">
+		</form> 
+		<?PHP
+			$ID = $_GET['videoID'];
+			print ("now playing: ".$ID);
+			#$ID = "OWc1jaycOlQ";
+			echo"<iframe width='560' height='315' src='https://www.youtube.com/embed/".$ID."?&autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
 		?>
 	</body>
 </html>
