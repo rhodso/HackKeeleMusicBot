ec43758c6c774e23a720e7cdc37369c6918d919b
Rory Gee <rorygeesbe@gmail.com>
2019-12-06 21:55:34 +0000

    "?>"

--- src/index.php
+++ src/index.php
@@ -8,6 +8,7 @@
     </head>
     <body>
 		<?php
-			echo"<iframe width='560' height='315' src='https://www.youtube.com/embed/"+someVariable+"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
-    </body>
+			#echo"<iframe width='560' height='315' src='https://www.youtube.com/embed/"+someVariable+"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+		?>
+	</body>
 </html>
