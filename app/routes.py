ecc5e8b99a0dd2b6d4e25d224feb4d7868e7c9d4
RoryGee <rorygee@outlook.com>
2019-12-07 02:48:00 +0000

    Additional logic for registration

--- app/routes.py
+++ app/routes.py
@@ -6,24 +6,32 @@ from app.forms import UsernameForm, IDForm
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if 'username' in session:
+        videos = [
+           {
+               'votes': 1,
+               'UID': {'Username': 'RoryGee'},
+                'VideoID': 'OWc1jaycOlQ'
+            },
+            {
+               'votes': 1,
+               'UID': {'Username': 'Rhodso'},
+               'VideoID': 'OWc1jaycOlQ'
+           }
+           ]
+        curVideo = next(iter(videos))
+    else:
+        return redirect('/register')
+    return render_template('index.html', title='HACK Keele | Music Player', videos=videos, curVideo=curVideo, name=session.get('username'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     form = UsernameForm()
     if form.validate_on_submit():
         session['username'] = form.userName.data
         return redirect('/')
-    videos = [
-        {
-            'votes': 1,
-            'UID': {'Username': 'RoryGee'},
-            'VideoID': 'OWc1jaycOlQ'
-        },
-        {
-            'votes': 1,
-            'UID': {'Username': 'Rhodso'},
-            'VideoID': 'OWc1jaycOlQ'
-        }
-    ]
-    curVideo = next(iter(videos))
-    return render_template('index.html', title='HACK Keele | Music Player', form=form, videos=videos, curVideo=curVideo, name=session.get('username'))
+    return render_template('register.html', title='HACK Keele | Music Player', form=form)
 
 
 @app.route('/requestSong', methods=['GET', 'POST'])
