c5d1c66a362293c94d9c9936ae70486902d6e0aa
rhodso <rhodsoprivate@gmail.com>
2022-10-19 11:31:15 +0100

    Fixed request page

--- web/request.php
+++ web/request.php
@@ -1,15 +1,14 @@
 <?php 
     include 'header.php'; 
     require_once 'db.php';
-?>
+    ?>
 
 <?php
     function showError($message) {
-        echo '<p class="error">' . $message . '</p>';
+        echo '<p class="error">' . $message . '</p><br><a href=index.php>Return to home page</a>';
     }
-    function showLog($message) {
-        echo '<p class="log">' . $message . '</p>';
-    }
+    // Set the request timeout
+    $requestTimeout = 3600;
 
     // Ensure the user is logged in
     if (!isset($_SESSION['user_id'])) {
@@ -35,7 +34,7 @@
 
     // Get the user's request
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        showLog('Request submitted');
+        
 
         // Check that $song is set
         if (!isset($_POST['song'])){
@@ -43,20 +42,14 @@
             exit;
         }
 
-        showLog('Song set');
-
         // Handle the form submission
         $songUrl = $_POST['song'];
-
-        showLog('Song URL set');
 
         // Detect funny business
         if (detectFunnyBusiness($songUrl, "string")) {
             showError('Nice try, but no banana ;)');
             exit;
         }
-        
-        showLog('No funny business detected');
 
         // Check that the song URL is not empty
         if(empty($songUrl)) {
@@ -65,16 +58,12 @@
             exit;
         }
 
-        showLog('Song URL not empty');
-
         // Check that the song URL is valid
         if (!filter_var($songUrl, FILTER_VALIDATE_URL)) {
             // If the song URL is not valid, show an error
             showError('Please enter a valid URL');
             exit;
         }
-
-        showLog('Song URL valid');
 
         // Check that the song URL is a youtube video, m.youtube.com, or youtu.be
         if (strpos($songUrl, 'youtube.com') === false && strpos($songUrl, 'youtu.be') === false && strpos($songUrl, 'm.youtube.com') === false) {
@@ -83,26 +72,80 @@
             exit;
         }
 
-        showLog('Song URL is youtube video');
+        // Check if song has been requested before
+        $stmt = $db -> prepare ('SELECT * FROM song WHERE Song_Url = :songUrl');
+        $stmt -> bindParam(':songUrl', $songUrl);
+        $stmt -> execute();
+        $song = $stmt -> fetch(PDO::FETCH_ASSOC);
+        
+        // If $song is empty, insert the song into the database
+        if (!$song) {
 
-        // var dump here and exit 
-        var_dump($songUrl);
+            // Get the title of the song from the youtube api
+            $songTitle = "";
+
+            // TODO, set to id (remove the youtube.com/watch?v= part)
+            $songTitleParts = explode("=", $songUrl);
+            $songTitle = $songTitleParts[1];
+
+            $songInitTime = 1;
+            
+            // Insert the song into the database
+            $sql = 'INSERT INTO Song (Song_LastRequest, Song_Url, Song_Title ) VALUES (:songLastRequest, :songUrl, :songTitle)';
+            $stmt = $db -> prepare ($sql);
+            $stmt -> bindParam(':songLastRequest', $songInitTime);
+            $stmt -> bindParam(':songUrl', $songUrl);
+            $stmt -> bindParam(':songTitle', $songTitle);
+            $stmt -> execute();
+
+            // Get the song ID
+            $stmt = $db -> prepare('SELECT * FROM Song WHERE Song_Url = :songUrl');
+            $stmt -> bindParam(':songUrl', $songUrl);
+            $stmt -> execute();
+            $sng = $stmt -> fetch(PDO::FETCH_ASSOC);
+
+            $songId = $sng['Song_ID'];
+        } else {
+
+            // Get the song ID
+            $songId = $song['Song_ID'];
+        }
+
 
         // Check when the song was last requested
-        // TODO
-        $lastRequest = 0;
+        $sql = "SELECT * FROM SongRequest WHERE Song_ID = :songId ORDER BY Request_Time DESC LIMIT 1";
+        $stmt = $db -> prepare ($sql);
+        $stmt -> bindParam(':songId', $songId);
+        $stmt -> execute();
+        $lastRequest = $stmt -> fetch(PDO::FETCH_ASSOC);
 
-        // If the song was requested less than an hour ago, show an error
-        if ($lastRequest > 0 && time() - $lastRequest < 3600) {
-            showError('Please wait at least an hour before requesting the same song again');
+        // If no request was found 
+        if (!$lastRequest) {
+            $lastRequestTime = 1;
+        } else {
+            $lastRequestTime = $lastRequest['Request_Time'];
+        }
+
+        // Check if the song has been requested in the last hour
+        if (time() - $lastRequestTime < $requestTimeout) {
+            // If the song has been requested in the last hour, show an error
+            showError('This song has already been requested in the last hour');
             exit;
         }
 
-        // Insert the song into the database
-        // TODO
+        $requestPlayed = 0;
 
+        // Insert the songrequest into the database
+        $sql = 'INSERT INTO SongRequest (User_ID, Song_ID, Request_Time, Request_Played) VALUES (:userId, :songId, :requestTime, :requestPlayed)';
+        $stmt = $db -> prepare ($sql);
+        $stmt -> bindParam(':userId', $_SESSION['user_id']);
+        $stmt -> bindParam(':songId', $songId);
+        $stmt -> bindParam(':requestTime', time());
+        $stmt -> bindParam(':requestPlayed', $requestPlayed);
+        $stmt -> execute();
+        
         // Redirect to the home page
-        // header('Location: index.php');
+        header('Location: index.php');
     }
 ?>
 
